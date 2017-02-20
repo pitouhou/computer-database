a53dd51f1b68a49523b2217ba1fd7fb96701343c
Pierre Mauthes <pmauthes@excilys.com>
2017-02-20 15:50:00 +0100

    ajout implémentation des DAO Computer et Company vide

---
+++ src/com/computerDatabase/DAO/CompanyDaoImpl.java
@@ -0,0 +1,15 @@
+package com.computerDatabase.DAO;
+
+import java.util.Collection;
+
+import com.computerDatabase.model.Company;
+
+public class CompanyDaoImpl implements CompanyDao {
+
+	@Override
+	public Collection<Company> list() throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/com/computerDatabase/DAO/ComputerDaoImpl.java
@@ -0,0 +1,39 @@
+package com.computerDatabase.DAO;
+
+import java.util.Collection;
+
+import com.computerDatabase.model.Computer;
+
+public class ComputerDaoImpl implements ComputerDao {
+
+	@Override
+	public void create(Computer computer) throws DAOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(int id) throws DAOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(Computer computer) throws DAOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Collection<Computer> list() throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Computer details(int id) throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
