953038c7c30892260db5a3d432d11e22eebe31f9
Pierre Mauthes <pmauthes@excilys.com>
2017-02-21 11:41:20 +0100

    ajout du package services et de la classe ComputerServices

---
+++ src/com/computerDatabase/services/ComputerServices.java
@@ -0,0 +1,59 @@
+package com.computerDatabase.services;
+
+import java.util.Collection;
+
+import com.computerDatabase.DAO.ComputerDao;
+import com.computerDatabase.DAO.DAOFactory;
+import com.computerDatabase.model.Computer;
+
+public class ComputerServices {
+	
+	public Collection<Computer> getComputerList(){
+		
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		ComputerDao comp = daoFactory.getComputerDao();
+				
+		Collection<Computer> comp1 = comp.list();		
+				
+		return comp1;
+		
+	}
+	
+	public Computer getComputerDetails(int id){
+		
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		ComputerDao comp = daoFactory.getComputerDao();
+				
+		Computer comp1 = comp.details(id);
+		
+		return comp1;
+	}
+	
+	public void addComputer(Computer computer){
+		
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		ComputerDao comp = daoFactory.getComputerDao();
+		
+		comp.create(computer);
+		
+	}
+	
+	public void updateComputer(Computer computer){
+		
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		ComputerDao comp = daoFactory.getComputerDao();
+		
+		comp.update(computer);
+		
+	}
+	
+	public void deleteComputer(int id){
+		
+		DAOFactory daoFactory = DAOFactory.getInstance();
+		ComputerDao comp = daoFactory.getComputerDao();
+		
+		comp.delete(id);
+		
+	}
+	
+}
