60b971ace4bd853227545d12f6fd02c44578b212
Pierre Mauthes <pmauthes@excilys.com>
2017-02-23 17:17:50 +0100

    Ajout du fichier DAO.properties et lecture des properties dans ConnectionManager

--- src/com/computerDatabase/DAO/ConnectionManager.java
+++ src/com/computerDatabase/DAO/ConnectionManager.java
@@ -1,22 +1,57 @@
 package com.computerDatabase.DAO;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Optional;
+import java.util.Properties;
 
 public class ConnectionManager {
 
 	private static Connection connect;
 	
 	public static Connection getInstance(){
-		if(connect == null){
-			try {
-				connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/computer-database-db?zeroDateTimeBehavior=convertToNull", "admincdb", "qwerty1234");
-			} catch (SQLException e) {
-				e.printStackTrace();
-			}
-		}		
-		return connect;	
+		try {
+			Optional<Properties> propertiesTmp = ConnectionManager.getProperties("DAO.properties");
+			Properties properties = propertiesTmp.get();
+			if(connect == null){
+				try {
+					connect = DriverManager.getConnection(properties.getProperty("url"), properties.getProperty("user"), properties.getProperty("password"));
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}		
+			return connect;	
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		return connect;
+	}
+	
+	public static Optional<Properties> getProperties(String fileName) throws FileNotFoundException, IOException{
+		
+		Properties properties = new Properties();
+		File	file = new File(fileName);
+		if(file.exists()){
+			FileInputStream input = new FileInputStream(fileName); 
+		    
+		      try{
+
+		         properties.load(input);
+		         return Optional.of(properties);
+
+		      }finally{
+
+		         input.close();
+
+		      }
+		      
+		}
+		return Optional.empty();
 	}
 	
 }
